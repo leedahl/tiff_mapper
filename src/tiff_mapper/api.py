8b2f25a8cbaa9a10ba1b9593220190c8b9d84e24
Michael Leedahl <michael@leedahl.com>
2024-12-21 21:26:59 -0700

    change ray initialize

--- src/tiff_mapper/api.py
+++ src/tiff_mapper/api.py
@@ -27,7 +27,7 @@ def mapper(input_path: str, output_path: str, ray_address: Optional[str]) -> Non
 
     if not ray.is_initialized():
         if ray_address is not None:
-            ray.init(f'_node_ip_address={ray_address}')
+            ray.init(_node_ip_address=ray_address)
 
         else:
             ray.init()
